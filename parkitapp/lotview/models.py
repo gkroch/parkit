2793f96fd36672a7008379cfd47c85b09f981e02
gkroch <gmk15@duke.edu>
2016-11-17 12:38:00 -0500

    Added templates, database classes completed

--- parkitapp/lotview/models.py
+++ parkitapp/lotview/models.py
@@ -2,7 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+class Lot(models.Model):
+    name = models.CharField(max_length=50)
+    spots_remaining = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Node(models.Model):
+    location = models.CharField(max_length=50)
+    lot = models.ForeignKey(Lot)
+
+    # Net change of entries and exits via node
+    def net_change():
+        pass
+
+    def __str__(self):
+        return self.location
+
 class SensorEvent(models.Model):
+    DIRECTIONS = (
+        ('e','Entry'),
+        ('x','Exit'),
+        ('u','Unknown'),
+        )
+    
     timestamp = models.DateTimeField('Timestamp')
-    node = models.IntegerField('Origin of signal')
-    direction = models.CharField(max_length=10) # Entry / Exit / Unknown
+    node = models.ForeignKey(Node)
+    direction = models.CharField(max_length=1, choices=DIRECTIONS) 
+
+    def __str__(self):
+        return str(self.node) + " " + str(self.timestamp) + " " + \
+            str(self.direction)
+    
--- parkitapp/lotview/views.py
+++ parkitapp/lotview/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+import datetime
 
+from .models import SensorEvent, Node, Lot
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'lotview/index.html')
+
+    l = Lot.objects.get(pk=1)
+    
+    context = {'spots_remaining' : l.spots_remaining}
+
+    return render(request, 'lotview/index.html', context)
+
+@require_http_methods(["POST","GET"])
+def data(request, nodeID, direct):
+    time = datetime.datetime.now() # Get current time
+
+    # Create sensor event
+    tempnode = Node.objects.get(pk=nodeID)
+    sensorevent = SensorEvent(timestamp=time,node=tempnode,direction=direct)
+    sensorevent.save()
+
+    context = {
+        'sensorevent' : sensorevent,
+        'timestamp'   : time,
+        'direction'   : direct,
+    }
+
+    return render(request, 'lotview/data.html', context)
